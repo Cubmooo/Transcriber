c5100da157af6703d568af1e9652e8f3ba0ab352
Cubmooo <Cubchoo.moo@gmail.com>
2026-04-22 09:09:58 +1200

    Initial commit all it contains is a single c++ file called transcriber. it outputs hello world

---
+++ transcriber.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string input;
+    std::cin >> input;
+    std::string name;
+    name = "Hello world";
+    std::cout << name << std::endl << input;
+    
+    return 0;
+}
