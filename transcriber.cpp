755a2f2519be8fe6b3e814b3bcd75b0154801d72
Joshua Davies <cubchoo.moo@gmail.com>
2026-04-27 16:01:19 +1200

    The functions for the inputting and interperting sound

--- transcriber.cpp
+++ transcriber.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <string>
+#include <thread>
 
 int main(){
-    std::string input;
-    std::cin >> input;
-    std::string name;
-    name = "Hello world";
-    std::cout << name << std::endl << input;
+    std::thread mic(fetchInput);
+    std::thread FftThread(FFT);
+    std::thread FftAnalyser(secondsToBeats);
+    std::thread UI(InitialiseUI);
+    std::thread UIInteraction(UIButtons);
     
     return 0;
+}
+
+int fetchInput(){
+    return 0;
+}
+
+int FFT(){
+    return 0;
+}
+
+int secondsToBeats(){
+    return 0;
+}
+
+void InitialiseUI(){
+    return;
+}
+
+void UIButtons(){
+
 }
