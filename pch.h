f0c100cf55a99156db5fe1532f2b92d677c569f7
Joshua Davies <cubchoo.moo@gmail.com>
2026-04-30 21:47:04 +1200

    The code has been updated to display the raw data from the microphone. There has also been an optimisation change pch has been used to precompile headers to speed up build time

---
+++ pch.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "portaudio.h"
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
--- transcriber.cpp
+++ transcriber.cpp
@@ -1,20 +1,6 @@
-#include <iostream>
-#include <string>
-#include <thread>
-
-int main(){
-    std::thread mic(fetchInput);
-    std::thread FftThread(FFT);
-    std::thread FftAnalyser(secondsToBeats);
-    std::thread UI(InitialiseUI);
-    std::thread UIInteraction(UIButtons);
-    
-    return 0;
-}
-
-int fetchInput(){
-    return 0;
-}
+#include "pch.h"
+#define SAMPLE_RATE 16000
+#define BUFFER_SIZE 512
 
 int FFT(){
     return 0;
@@ -29,5 +15,75 @@ void InitialiseUI(){
 }
 
 void UIButtons(){
+    return;
+}
 
+int fetchInput(){
+    PaError err = Pa_Initialize();
+
+    PaDeviceIndex inputDevice = Pa_GetDefaultInputDevice();
+    std::cout << inputDevice;
+    std::cout << "mic";
+    if (inputDevice == paNoDevice) {
+        fprintf(stderr, "no mic");
+        Pa_Terminate();
+        return -1;
+    }
+
+    int numDevices = Pa_GetDeviceCount();
+    for (int i = 0; i < numDevices; i++) {
+        const PaDeviceInfo* info = Pa_GetDeviceInfo(i);
+        if (info->maxInputChannels > 0) {
+            printf("[%d] %s (inputs: %d)\n", i, info->name, info->maxInputChannels);
+    }
+}
+
+     PaStream* stream;
+    Pa_OpenDefaultStream(&stream,
+        1,
+        0, 
+        paInt32,
+        SAMPLE_RATE,
+        BUFFER_SIZE,
+        nullptr,
+        nullptr
+    );
+
+    Pa_StartStream(stream);
+
+    std::vector<int16_t> buffer(BUFFER_SIZE);
+
+    while (true) {
+        Pa_ReadStream(stream, buffer.data(), BUFFER_SIZE);
+        
+        for (int i = 0; i < BUFFER_SIZE; i++) {
+            std::cout << buffer[i] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    Pa_StopStream(stream);
+    Pa_CloseStream(stream);
+    Pa_Terminate();
+
+    std::cout << "After Pa_Initialize" << std::endl;
+    return 0;
+}
+
+int main(){
+    std::cerr << "MAIN STARTED\n";
+    std::cout << "Hello World!"<< std::endl;
+    std::thread mic(fetchInput);
+    std::thread FftThread(FFT);
+    std::thread FftAnalyser(secondsToBeats);
+    std::thread UI(InitialiseUI);
+    std::thread UIInteraction(UIButtons);
+    
+    mic.join();
+    FftThread.join();
+    FftAnalyser.join();
+    UI.join();
+    UIInteraction.join();
+
+    return 0;
 }
